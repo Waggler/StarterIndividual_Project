ce71ba2a3f11eb74f73be153d8177938a6e3ad35
Waggler <khdavis4@gmail.com>
2021-01-19 22:02:52 -0500

    Blank
    - Remove some files

---
+++ Starter_Individual_Project/Assets/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    //private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (Input.GetKey("escape")) //if escape key is pressed end game
+        {
+            Application.Quit();
+        }
+
+        //transform.position = player.transform.position + offset;
+    }
+}
